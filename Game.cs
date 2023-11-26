ad86d673ceebf09f9867fab1a4c4c21facd5c18f
Hasan Abdulrazzak <hasan.fuaad02@gmail.com>
2023-11-26 19:50:37 +0300

    nin_Commit

---
+++ Game.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Maze_Game
+{
+    class Game
+    {
+
+        private World MyWorld;
+        private Player CurrentPlayer;
+        public void start()
+        {
+
+
+            //SetCursorPosition(4, 2);
+            //Write("x");
+
+            String[,] grid = {
+           { "=","=","=","=" ,"=" ,"=","=" },
+           { "="," ","="," " ," " ," ","=" },
+           { " "," ","="," " ,"=" ," ","=" },
+           { "="," ","="," " ,"=" ," ","=" },
+           { "="," "," "," " ,"=" ,"X","=" },
+           { "=","=","=","=" ,"=" ,"=","=" },
+
+
+        };
+             MyWorld = new World(grid);
+             CurrentPlayer = new Player(0,2);
+            RunGameLoop();           
+
+
+
+
+          
+            ReadKey(true);
+        }
+        private void DisplyIntro()
+        {
+            WriteLine("Welcome to the Maze Game !");
+            WriteLine("\n Instructions");
+            WriteLine(">> Use the arrow keys tom move ");
+            Write(">>Try to reach the target, which look like theis: ");
+            ForegroundColor = ConsoleColor.Green;
+            WriteLine("x");
+            ResetColor();
+            WriteLine(">> Press any key to start");
+            ReadKey(true);
+        }
+
+        private void DisplyOutro()
+        {
+            Clear();
+            WriteLine("You escape!");
+            WriteLine("Thanks for playing");
+            WriteLine("Press any key to exit...");
+            ReadKey(true);
+
+        }
+
+        private void DrawFrame()
+        {
+            Clear();
+            MyWorld.Draw();
+            CurrentPlayer.Draw();
+        }
+
+        private void HandlePlayerInput()
+        {
+            ConsoleKeyInfo keyInfo = ReadKey(true);
+            ConsoleKey key = keyInfo.Key;
+
+                switch (key)
+                {
+                case ConsoleKey.UpArrow:
+                    if (MyWorld.IsPositionWalkable(CurrentPlayer.X, CurrentPlayer.Y - 1))
+                    {
+                        CurrentPlayer.Y -= 1;
+                    }
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (MyWorld.IsPositionWalkable(CurrentPlayer.X, CurrentPlayer.Y + 1))
+                    {
+                        CurrentPlayer.Y += 1;
+                    }
+                    break;
+                case ConsoleKey.LeftArrow:
+                    if (MyWorld.IsPositionWalkable(CurrentPlayer.X - 1, CurrentPlayer.Y ))
+                    {
+                        CurrentPlayer.X -= 1;
+                    }
+                    break;
+                case ConsoleKey.RightArrow:
+                    if (MyWorld.IsPositionWalkable(CurrentPlayer.X + 1, CurrentPlayer.Y ))
+                    {
+                        CurrentPlayer.X += 1;
+                    }
+                    break;
+                default:
+                    break;
+                 }
+
+        }
+        //run for ever
+        private void RunGameLoop()
+        {
+            DisplyIntro();
+            while (true)
+            {
+                //draw evreything
+
+                DrawFrame();
+                // check for player input from the keyboard and move the player
+                HandlePlayerInput();
+
+                //check if the player has reached the target and end the game if so
+                string elementAtPlayerPos = MyWorld.GetElementAt(CurrentPlayer.X, CurrentPlayer.Y);
+                if (elementAtPlayerPos == "X")
+                {
+                 
+                    break;
+                }
+                //todo!
+
+
+
+                //give the console a chance to render
+                System.Threading.Thread.Sleep(30);
+            }
+            DisplyOutro();
+        }
+    }
+}
---
+++ World.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Maze_Game
+{
+    class World
+    {
+        private string[,] Grid;
+        private int Rows;
+        private int Cols;
+
+        public World(string[,] grid)
+        {
+            Grid = grid;
+            Rows = Grid.GetLength(0);
+            Cols = Grid.GetLength(1);
+
+        }
+
+        public void Draw()
+        {
+            for(int x = 0; x < Rows; x++)
+            {
+                for(int y = 0; y < Cols; y++)
+                {
+                    string element = Grid[x, y];
+                    SetCursorPosition(y, x);
+                    Write(element);
+                }
+            }
+        }
+
+        public string GetElementAt(int x, int y)
+        {
+            return Grid[y,x];
+        }
+        internal bool IsPositionWalkable(int x, int y)
+        {
+            if (x < 0 || y < 0 || x>= Rows || y>= Cols)
+            {
+                return false;
+            }
+            return Grid[y, x] == " " || Grid[y, x] == "X";
+        }
+        
+           
+        
+    }
+}
